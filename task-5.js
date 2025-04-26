675d1b7a87871ab160a86ee783bcdd67370f1917
Timofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-04-26 12:28:04 +0300

    Js-7HommeWork

--- task-5.js
+++ task-5.js
@@ -1,14 +1,15 @@
-// Завдання 5: Зміна кольору
-const changeColorButton = document.querySelector(".change-color");
-const body = document.body;
-const colorSpan = document.querySelector(".color");
+/* Завдання 5: Зміна кольору фону */
+const colorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+colorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
-changeColorButton.addEventListener("click", () => {
-  const randomColor = getRandomHexColor();
-  body.style.backgroundColor = randomColor;
-  colorSpan.textContent = randomColor;
-});
+
